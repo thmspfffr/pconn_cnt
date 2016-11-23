1d3c7e2b961c1dfd2342e6722bfa2fe81fe1f82a
thmspfffr <thpfeffer@gmx.de>
2016-11-23 17:47:04 +0100

    final analyses

--- pconn_cnt_src_dfa.m
+++ pconn_cnt_src_dfa.m
@@ -41,7 +41,7 @@ for v = [2 8]
     is_src    = 0;
     fsample   = 400;
     SUBJLIST  = [4 5 6 7 8 9 10 11 12 13 15 16 17 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34];
-    foi       = [2 4; 4 8; 8 12; 12 36; 50 100];
+    foi       = [2 4; 4 8; 8 12; 12 36];
     i_fit     = [2 50];
     i_calc    = [1.5 70];
     dfa_overlap = 0.5;
@@ -134,7 +134,7 @@ for v = [2 8]
     is_src    = 0;
     fsample   = 400;
     SUBJLIST  = [4 5 6 7 8 9 10 11 12 13 15 16 17 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34];
-    foi       = [2 4; 4 8; 8 12; 12 36; 50 100];
+    foi       = [2 4; 4 8; 8 12; 12 36];
     i_fit     = [5 75];
     % i_calc    = [1.5 50];
     dfa_overlap = 0.5;
@@ -213,97 +213,61 @@ for v = [2 8]
             
           elseif strcmp(filt,'eloreta')
             if strcmp(gridsize,'xcoarse')
-              % THIS TAKES THE AVERAGE CROSS SPECTRUM IN SOURCE SPACE
-              % TO DERIVE DIPOLE ORIENTATION!!!
-              for i = 1 : 3
-                
-                load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,i,iblock,2)]);
-                load(['~/pconn_cnt/proc/src/' sprintf('pconn_cnt_sens_cs_s%d_m%d_b%d_f%d_v%d.mat',isubj,i,iblock,freq,1)]);
-                
-                % watch out! L_coarse is correct even for cortex grid
-                A_tmp = mkfilt_eloreta_v2(sa.L_coarse);
-                
-                eval(sprintf('A%d = A_tmp;',i)); clear A_tmp
-                eval(sprintf('cs%d = nanmean(cs(:,:,foi(ifoi,1):foi(ifoi,2)),3);',i))
-                
-              end
-              
+              % --------------------------------------------
+              % XCOARSE GRID 
+              % --------------------------------------------
               load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,m,iblock,2)]);
               A = mkfilt_eloreta_v2(sa.L_coarse);
               
-              for i = 1 : size(A1,2)
-                Aloc = squeeze(A(:,i,:));
-                cs_src1 = squeeze(A1(:,i,:))'*cs1*squeeze(A1(:,i,:)); 
-                cs_src2 = squeeze(A2(:,i,:))'*cs2*squeeze(A2(:,i,:));
-                cs_src3 = squeeze(A3(:,i,:))'*cs3*squeeze(A3(:,i,:));
-                cs_src = (cs_src1+cs_src2+cs_src3)./3;
-                [u s v] = svd(real(cs_src));
-                f(:,i)=Aloc*u(:,1);
+              load(sprintf(['~/pconn_cnt/proc/src/' 'pconn_cnt_src_common_filter_s%d_f%d_v%d.mat'],isubj,ifoi,1));
+  
+              for ivox = 1 : size(A,2)
+
+                Aloc       = squeeze(A(:,ivox,:));
+                [u s vv]   = svd(real(cs_src_all(:,:,ivox)));
+                A1(:,ivox) = Aloc*u(:,1);
+
               end
-              clear A1 A2 A3 cs1 cs2 cs3
-              A1 = f; clear f
+              
+              clear A 
               
             elseif strcmp(gridsize,'coarse')
-              % THIS TAKES THE AVERAGE CROSS SPECTRUM IN SOURCE SPACE
-              % TO DERIVE DIPOLE ORIENTATION!!!
-              for i = 1 : 3
-                
-                load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,i,iblock,1)]);
-                load(['~/pconn_cnt/proc/src/' sprintf('pconn_cnt_sens_cs_s%d_m%d_b%d_f%d_v%d.mat',isubj,i,iblock,freq,1)]);
-                
-                % watch out! L_coarse is correct even for cortex grid
-                A_tmp = mkfilt_eloreta_v2(sa.L_coarse);
-                
-                eval(sprintf('A%d = A_tmp;',i)); clear A_tmp
-                eval(sprintf('cs%d = nanmean(cs(:,:,foi(ifoi,1):foi(ifoi,2)),3);',i))
-                
-              end
-              
+              % --------------------------------------------
+              % COARSE GRID 
+              % --------------------------------------------
               load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,m,iblock,1)]);
               A = mkfilt_eloreta_v2(sa.L_coarse);
               
-              for i = 1 : size(A1,2)
-                Aloc = squeeze(A(:,i,:));
-                cs_src1 = squeeze(A1(:,i,:))'*cs1*squeeze(A1(:,i,:)); 
-                cs_src2 = squeeze(A2(:,i,:))'*cs2*squeeze(A2(:,i,:)); 
-                cs_src3 = squeeze(A3(:,i,:))'*cs3*squeeze(A3(:,i,:)); 
-                cs_src = (cs_src1+cs_src2+cs_src3)./3;
-                [u s v] = svd(real(cs_src));
-                f(:,i)=Aloc*u(:,1);
+              load(sprintf(['~/pconn_cnt/proc/src/' 'pconn_cnt_src_common_filter_s%d_f%d_v%d.mat'],isubj,ifoi,1));
+  
+              for ivox = 1 : size(A,2)
+
+                Aloc       = squeeze(A(:,ivox,:));
+                [u s vv]   = svd(real(cs_src_all(:,:,ivox)));
+                A1(:,ivox) = Aloc*u(:,1);
+
               end
-              clear A1 A2 A3 cs1 cs2 cs3
-              A1 = f; clear f
               
+              clear A 
             elseif strcmp(gridsize,'cortex')
-              % THIS TAKES THE AVERAGE CROSS SPECTRUM IN SOURCE SPACE
-              % TO DERIVE DIPOLE ORIENTATION!!!
-              for i = 1 : 3
-                
-                load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,i,iblock,3)]);
-                load(['~/pconn_cnt/proc/src/' sprintf('pconn_cnt_sens_cs_s%d_m%d_b%d_f%d_v%d.mat',isubj,i,iblock,freq,1)]);
-                
-                % watch out! L_coarse is correct even for cortex grid
-                A_tmp = mkfilt_eloreta_v2(sa.L_coarse);
-                
-                eval(sprintf('A%d = A_tmp;',i)); clear A_tmp
-                eval(sprintf('cs%d = nanmean(cs(:,:,foi(ifoi,1):foi(ifoi,2)),3);',i))
-                
-              end
-              
+              % --------------------------------------------
+              % CORTEX GRID 
+              % --------------------------------------------
+             
               load([indir sprintf('pconn_cnt_sa_s%d_m%d_b%d_v%d.mat',isubj,m,iblock,3)]);
               A = mkfilt_eloreta_v2(sa.L_coarse);
               
-              for i = 1 : size(A1,2)
-                Aloc = squeeze(A(:,i,:));
-                cs_src1 = squeeze(A1(:,i,:))'*cs1*squeeze(A1(:,i,:));
-                cs_src2 = squeeze(A2(:,i,:))'*cs2*squeeze(A2(:,i,:));
-                cs_src3 = squeeze(A3(:,i,:))'*cs3*squeeze(A3(:,i,:));
-                cs_src = (cs_src1+cs_src2+cs_src3)./3;
-                [u s v] = svd(real(cs_src));
-                f(:,i)=Aloc*u(:,1);
+              load(sprintf(['~/pconn_cnt/proc/src/' 'pconn_cnt_src_common_filter_s%d_f%d_v%d.mat'],isubj,ifoi,1));
+  
+              for ivox = 1 : size(A,2)
+
+                Aloc       = squeeze(A(:,ivox,:));
+                [u s vv]   = svd(real(cs_src_all(:,:,ivox)));
+                A1(:,ivox) = Aloc*u(:,1);
+
               end
-              clear A1 A2 A3 cs1 cs2 cs3
-              A1 = f; clear f
+              
+              clear A 
             end
           end
           
@@ -351,8 +315,7 @@ for v = [2 8]
           
           
           clear ampenv ampenv1
-          
-          
+
         end
         
         save(sprintf([outdir 'pconn_cnt_src_dfa_s%d_m%d_f%d_v%d.mat'],isubj,m,ifoi,v),'par','-v7.3');
@@ -368,7 +331,7 @@ error('STOP')
 outdir   = '/home/tpfeffer/pconn_cnt/proc/dfa/';
 
 cnt = 0;
-v = 8;
+v = 2;
 cnt_exist = 0;
 for m = 1 : 3
   for isubj = SUBJLIST
